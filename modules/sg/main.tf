0498667a23f77c636f641315de9b1e24c86afdd3
Bal1oon <ws32159@naver.com>
2025-11-16 13:55:02 +0900

    fix #21: backend_sg 필드 수정

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -47,7 +47,7 @@ resource "aws_security_group" "backend_sg" {
     from_port = 8000
     to_port = 10000
     protocol = "tcp"
-    security_groups = [var.vpc_cidr]
+    cidr_blocks = [var.vpc_cidr]
   }
 
   egress {
