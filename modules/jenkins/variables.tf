86296dd37ac34a57f4a7774dfd1a27ef72402096
Bal1oon <ws32159@naver.com>
2025-11-18 23:06:41 +0900

    feat #36: 도커용 볼륨 추가

--- modules/jenkins/variables.tf
+++ modules/jenkins/variables.tf
@@ -6,3 +6,11 @@ variable "key_name" {}
 variable "sg_id" {}
 variable "az" {}
 variable "prefix" {}
+variable "jenkins_ebs_size" { default = 50 }
+variable "jenkins_ebs_iops" { default = 3000 }
+variable "jenkins_ebs_throughput" { default = 125 }
+variable "jenkins_ebs_type" { default = "gp3" }
+variable "docker_ebs_size" { default = 50 }
+variable "docker_ebs_iops" { default = 6000 }
+variable "docker_ebs_throughput" { default = 125 }
+variable "docker_ebs_type" { default = "gp3" }
